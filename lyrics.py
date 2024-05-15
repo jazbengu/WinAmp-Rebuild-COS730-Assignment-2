649cf70ab5537c939ebe4919099f3d299192e7be
Joy Bengu <bengujoy@gmail.com>
2024-05-15 21:25:55 +0200

    removed the contributors and the embed text with the lyrics
    Signed-off-by: Joy Bengu <bengujoy@gmail.com>

--- lyrics.py
+++ lyrics.py
@@ -1,11 +1,23 @@
-import requests
+import re
+
 import lyricsgenius
+
+
 def fetch_lyrics(artist, song):
     # Create a LyricsGenius object with your Genius API access token
     genius = lyricsgenius.Genius('o2HZ6LDQo8zYrgn-6m3d8QkhbXY5QANj1IYWAw6LPbusV1Xzg5rcWSYIk1bnPKPL')
     song = genius.search_song(song, artist)
-    # Print the lyrics
-    return song.lyrics
 
+    # Extract only the lyrics and remove additional text using regular expressions
+    lyrics = song.lyrics
 
+    # Remove text from 'Contributors' to 'Lyrics'
+    lyrics = re.sub(r'\d+ Contributors.*?Lyrics', '', lyrics, flags=re.DOTALL)
 
+    # Remove text from 'Embed' to the end
+    lyrics = re.sub(r'Embed.*$', '', lyrics, flags=re.DOTALL)
+
+    # Strip any leading or trailing whitespace
+    lyrics = lyrics.strip()
+
+    return lyrics
