b5ec9f5ff777f356b2aa863861c4c1c863bfed85
Joy Azile Zoe Bengu <bengujoy@gmail.com>
2024-05-22 21:23:44 +0200

    Tidy Up Code
    Signed-off-by: Joy Azile Zoe Bengu <bengujoy@gmail.com>

--- Tests/test_gui.py
+++ Tests/test_gui.py
@@ -1,14 +1,47 @@
 import unittest
-from unittest.mock import patch, MagicMock
-from PyQt5.QtWidgets import QApplication, QTabWidget, QTextEdit
+import sys
+from PyQt5.QtWidgets import QApplication
+from main import WinampClone, PlaylistCreator
 
-class TestMinimal(unittest.TestCase):
-    def test_qt_text_edit(self):
-        app = QApplication([])
-        text_edit = QTextEdit()
-        text_edit.setText("Test")
-        self.assertEqual(text_edit.toPlainText(), "Test")
-        app.exit()
+
+class TestWinampIntegration(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.app = QApplication(sys.argv)
+
+    @classmethod
+    def tearDownClass(cls):
+        sys.exit()
+
+    def test_playlist_creation_and_playback(self):
+        # Create the WinampClone instance
+        winamp = WinampClone()
+
+        # Add some music to the playlist
+        winamp.add_music_from_directory("C:/Users/zozoj/Music")
+
+        # Simulate user interaction: Create a playlist
+        winamp.create_playlist_dialog()
+        playlist_creator_dialog = winamp.playlist_creator_dialog
+        playlist_creator_dialog.playlist_name_input.setText("Test Playlist")
+        playlist_creator_dialog.save_location = "C:/Users/zozoj/Music/test_playlist.json"
+        playlist_creator_dialog.create_playlist()
+
+        # Load the created playlist
+        winamp.load_playlist()
+
+        # Play the first song in the playlist
+        winamp.play()
+
+        # Verify that the current song label is updated
+        self.assertEqual(winamp.current_song_label.text(), "Now Playing: Nectar - BM ft Jay Park")
+
+        # Stop the playback
+        winamp.stop()
+
+        # Add more assertions as needed to test other functionalities
+
 
 if __name__ == '__main__':
     unittest.main()
